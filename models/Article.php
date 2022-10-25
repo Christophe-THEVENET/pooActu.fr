cbfb0e35c942a75a9c771d01a3c6814850b6cd16
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-10-25 17:02:48 +0200

    init projet + entité article

---
+++ models/Article.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+
+
+
+class Article
+{
+
+  private int $id;
+  private string $title;
+  private string $content;
+
+
+  public function __construct($id, $title, $content)
+  {
+    $this->id = $id;
+    $this->title = $title;
+    $this->content = $content;
+  }
+
+  
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+  
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
